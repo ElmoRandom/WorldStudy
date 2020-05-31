e7ef98d1f663fb58acd3f4b55df6996e20a1a34f
Elmo <elmoworld2005@gmail.com>
2020-05-31 12:19:08 +0700

    parrallax

---
+++ parrallax.js
@@ -0,0 +1,25 @@
+function parrallax(element, distance, speed){
+    const item = document.querySelector(element);
+
+    item.style.transform = 'translateY(' + (speed * distance) +  'px)';
+    console.log(distance);
+}
+
+function parrallaxX(element, distance, speed){
+    const item = document.querySelector(element);
+
+    item.style.transform = 'translateY(60vh)' + ' translateX(' + (speed * distance) + 'px)';
+}
+
+var elmo = document.querySelector('#president');
+var text = document.querySelector('.screen-container h1');
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY < 800){
+        parrallax('.screen-container', window.scrollY, 1);
+        parrallax('nav', window.scrollY,1);
+        text.style.transform = 'translateY(60vh)' + ' translateX(' + (window.scrollY) + 'px)';
+        elmo.style.transform = 'translateY(10vh)' + ' translateX(' + (window.scrollY) + 'px)';
+    }
+});
+
